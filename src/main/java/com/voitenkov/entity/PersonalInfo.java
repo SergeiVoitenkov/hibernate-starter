886728b53f97bd5631d257fdd19060223acd9eea
Sergei Voitenkov <Voitenkov.Sergei@gmail.com>
2022-01-30 13:43:37 +0300

    Embedded components

---
+++ src/main/java/com/voitenkov/entity/PersonalInfo.java
@@ -0,0 +1,25 @@
+package com.voitenkov.entity;
+
+import com.voitenkov.converter.BirthDayConverter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Embeddable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class PersonalInfo {
+
+    private String firstname;
+    private String lastname;
+
+    @Convert(converter = BirthDayConverter.class)
+    private BirthDay birthDate;
+}
